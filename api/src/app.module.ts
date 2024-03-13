f2e351cfbe50e453dbc4884da8cf8987205fa553
Gino Tapia <destroctorgt@gmail.com>
2024-03-13 10:04:20 -0500

    feat: :construction: creado módulos de auth y video-games

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { AuthService } from './auth/auth.service'
+import { AuthController } from './auth/auth.controller'
+import { VideoGamesService } from './video-games/video-games.service'
+import { VideoGamesController } from './video-games/video-games.controller'
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: []
+  controllers: [AuthController, VideoGamesController],
+  providers: [AuthService, VideoGamesService]
 })
 export class AppModule {}
---
+++ api/src/video-games/video-games.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common'
+
+@Controller('video-games')
+export class VideoGamesController {}
