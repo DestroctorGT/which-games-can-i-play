9657f8da93d94350223890f1b67378e25e5d3bb8
Gino Tapia <destroctorgt@gmail.com>
2024-03-13 09:47:41 -0500

    docs: :recycle: Preparando el código para el desarrollo

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -17,9 +17,33 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 200,
+        trailingComma: 'none',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'auto'
+      }
+    ],
+    'import/no-default-export': 'off',
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: '*', next: ['return', 'export'] },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] }
+    ],
+    '@typescript-eslint/no-extraneous-class': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off'
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('API Which Games Can I Play')
+    .setDescription('Platform where users can search for video games compatible with their system.')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api/docs', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true
+    }
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+  await app.listen(4000)
 }
-bootstrap();
+bootstrap()
